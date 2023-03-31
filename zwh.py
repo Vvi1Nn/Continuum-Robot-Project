8b12e4596b42775d4f7e133a95c712acc1a96646
Vvi1Nn <2638569699@qq.com>
2023-03-31 23:18:32 +0800

    一个测试文件

--- zwh.py
+++ zwh.py
@@ -2,25 +2,10 @@
 
 print("测试")
 
-def __int2hex(value_int):
-    
-    if type(value_int) != int or value_int > 0x7FFFFFFF or value_int < -0x80000000:
-        print("[int2hex] value 错误!!!")
-        return False
-    if value_int >= 0:
-        value_str = hex(value_int & 0xFFFFFFFF)[2:].upper()
-        if len(value_str) < 8:
-            value_str = '0'*(8-len(value_str)) + value_str
-    else:
-        value_str = hex(int(bin(- value_int ^ 0xFFFFFFFF), 2) + 1)[2:].upper()
-    # value_str = hex(value_int)[2:].upper()
-    # if len(value_str) < 8:
-    #     value_str = '0'*(8-len(value_str)) + value_str
-    value_list = [0x0] * 4
-    value_list[0] = value_str[6:8]
-    value_list[1] = value_str[4:6]
-    value_list[2] = value_str[2:4]
-    value_list[3] = value_str[0:2]
-    return value_str
+a = {"a":1,"b":2}
+b = a.items()
+for i in b:
+    print(i[1])
 
-print(__int2hex(10000))
+a = "SDO_R"
+print(a[4:5])
