0784994a2624813979f93164e1472ebaedffe065
Vvi1Nn <2638569699@qq.com>
2023-04-06 15:41:28 +0800

    GUI测试，通过

--- gui/test.py
+++ gui/test.py
@@ -11,30 +11,28 @@ sys.path.append(BASE_DIR)
 import motor.function as motor_func
 import usbcan.function as usbcan_func
 
-isAction = True
 
-def callback():
-    print("你点了一下按钮")
-
-def addnum():
-    num = int(motor_button_1.cget("text")) #获取组件的参数选项
-    motor_button_1.config(text=str(num + 1))
 
 def action_1():
-    if isAction:
-        usbcan_1 = usbcan_func.UsbCan()
-        motor_1 = motor_func.Motor(usbcan_1, 1, "position_control")
-        isAction = False
     motor_1.set_position(10)
     motor_1.execute()
+
+def action_2():
+    motor_2.set_position(10)
+    motor_2.execute()
+
 
 main_gui = tk.Tk()
 main_gui.title("【连续体机器人】控制面板")
 main_gui.geometry("2560x1600") # 全屏
 main_gui.config(bg = "#FFFFFF") # 背景：白色
 
-motor_button_1 = tk.Button(main_gui, text = "1", command = addnum, activeforeground = "blue", activebackground = "yellow", repeatdelay = 50, repeatinterval = 50)
-motor_button_2 = tk.Button(main_gui, text = "No.2", command = callback, activeforeground = "blue", activebackground = "yellow")
+usbcan_1 = usbcan_func.UsbCan()
+motor_1 = motor_func.Motor(usbcan_1, 1, "position_control")
+motor_2 = motor_func.Motor(usbcan_1, 2, "position_control")
+
+motor_button_1 = tk.Button(main_gui, text = "No.1", command = action_1, activeforeground = "blue", activebackground = "yellow", repeatdelay = 25, repeatinterval = 1)
+motor_button_2 = tk.Button(main_gui, text = "No.2", command = action_2, activeforeground = "blue", activebackground = "yellow", repeatdelay = 25, repeatinterval = 1)
 
 motor_button_1.grid(row = 0, column = 0, sticky = "nwse")
 motor_button_2.grid(row = 1, column = 0, sticky = "nwse")
