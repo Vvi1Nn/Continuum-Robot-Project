0b167e2a10aa3121d79aa6d2de87d57151fef455
Vvi1Nn <2638569699@qq.com>
2023-04-11 18:38:53 +0800

    更新控制字和状态字

--- motor/protocol.py
+++ motor/protocol.py
@@ -27,22 +27,65 @@ NMT_STATUS = {"boot_up"        : 0,
               "pre-operational": 127,
               }
 
-CMD_T = {"read"   : 0x40,
-        "write_32": 0x23,
-        "write_16": 0x2B,
-        "write_8" : 0x2F,
-        }
+CMD_T = {"read"    : 0x40,
+         "write_32": 0x23,
+         "write_16": 0x2B,
+         "write_8" : 0x2F,
+         }
 
-CMD_R = {"read_32": 0x43,
-        "read_16" : 0x4B,
-        "read_8"  : 0x4F,
-        "write"   : 0x60,
-        "error"   : 0x80,
-        }
+CMD_R = {"read_32" : 0x43,
+         "read_16" : 0x4B,
+         "read_8"  : 0x4F,
+         "write"   : 0x60,
+         "error"   : 0x80,
+         }
 
-CONTROL_WORD = {}
+CONTROL_WORD_BIT = {"switch_on"       : {"bit": 0, False: 0, True: 1},
+                   "enable_voltage"   : {"bit": 1, False: 0, True: 1},
+                   "quick_stop"       : {"bit": 2, False: 1, True: 0},
+                   "enable_operation" : {"bit": 3, False: 0, True: 1},
+                   "action"           : {"bit": 4, False: 0, True: 1},
+                   "is_immediate"     : {"bit": 5, False: 0, True: 1},
+                   "is_relative"      : {"bit": 6, False: 0, True: 1},
+                   "fault_reset"      : {"bit": 7, False: 0, True: 1},
+                   }
 
-STATUS_WORD = {}
+CONTROL_WORD = {"reset"             : 0b10000000, # 优先级 1
+                "power_off"         : 0b00000000, # 优先级 2
+                "quick_stop"        : 0b00000010, # 优先级 3
+                "servo_close"       : 0b00000110, # 优先级 4
+                "servo_ready_stop"  : 0b01100111, # 优先级 5 立即模式 相对运行
+                "servo_enable_start": 0b01111111, # 优先级 5 立即模式 相对运行
+               }
+
+STATUS_WORD_BIT = {"ready_to_switch_on"     : {"bit": 0, 0: False, 1: True},
+                   "switched_on"            : {"bit": 1, 0: False, 1: True},
+                   "operation_enabled"      : {"bit": 2, 0: False, 1: True},
+                   "fault"                  : {"bit": 3, 0: False, 1: True},
+                   "voltage_enabled"        : {"bit": 4, 0: False, 1: True},
+                   "quick_stop"             : {"bit": 5, 1: False, 0: True},
+                   "switch_on_disabled"     : {"bit": 6, 0: False, 1: True},
+                   "warning"                : {"bit": 7, 0: False, 1: True},
+                   "manufacturer"           : {"bit": 8, 0: None, 1: None},
+                   "remote"                 : {"bit": 9, 0: False, 1: True},
+                   "target_reached"         : {"bit": 10, 0: False, 1: True},
+                   "internal_limit_active"  : {"bit": 11, 0: False, 1: True},
+                   "set_point_acknowledge"  : {"bit": 12, 0: False, 1: True},
+                   "following_error"        : {"bit": 13, 0: False, 1: True},
+                   "manufacturer_specific_1": {"bit": 14, 0: None, 1: None},
+                   "manufacturer_specific_2": {"bit": 15, 0: None, 1: None},
+                   }
+
+STATUS_WORD = {"not_ready_to_switch_on": 0b0000000000000000,
+               "switch_on_disabled"    : 0b0000000001000000,
+               "ready_to_switch_on"    : 0b0000000000100001,
+               "switch_on"             : 0b0000000000100011,
+               "operation_enabled"     : 0b0000000000100111,
+               "quick_stop_active"     : 0b0000000000000111,
+               "fault_reaction_active" : 0b0000000000001111,
+               "fault"                 : 0b0000000000001000,
+               }
+                    
 
 CONTROL_MODE = {"position_control": 1,
                 "speed_control"   : 3,
@@ -54,10 +97,6 @@ OD = {"save_all"         : [0x1010, 0x01],
       "tpdo_1_timer"     : [0x1800, 0x05], # 定时间隔 单位:ms
       "tpdo_2_transtype" : [0x1801, 0x02],
       "tpdo_2_timer"     : [0x1801, 0x05],
-      "tpdo_3_transtype" : [0x1801, 0x02],
-      "tpdo_3_timer"     : [0x1801, 0x05],
-      "tpdo_4_transtype" : [0x1801, 0x02],
-      "tpdo_4_timer"     : [0x1801, 0x05],
       "control_word"     : [0x6040, 0x00],
       "status_word"      : [0x6041, 0x00], # RO
       "control_mode"     : [0x6060, 0x00],
