07cbed996b0f89c48dcb6bff60e0ec0da39d0240
Vvi1Nn <2638569699@qq.com>
2023-04-18 12:26:35 +0800

    UI接口

--- gui/function.py
+++ gui/function.py
@@ -2,6 +2,7 @@
 
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore
 
 from ui_login import Ui_MainWindow as Ui_LoginPanel
 from ui_control import Ui_MainWindow as Ui_ControlPanel
@@ -13,12 +14,28 @@ class LoginPanel(QMainWindow):
         self.ui = Ui_LoginPanel()
         self.ui.setupUi(self)
         
-        self.show()
-        self.__fix_window()
+        self.show() # 显示登录窗口
+        self.__fix_window_size() # 固定窗口大小
+        
+        self.__login_check() # 登录
     
-    def __fix_window(self, width = 1300, height = 631):
+    def __fix_window_size(self, width = 1300, height = 630) -> None:
         self.resize(width, height)
         self.setFixedSize(width, height)
+
+    def __login_check(self):
+        self.ui.button_login.clicked.connect(self.__jump_to_control_panel)
+
+    def __jump_to_control_panel(self):
+        account = self.ui.box_user.currentText()
+        password = self.ui.password.text()
+        if account == "测试人员" and password == "123":
+            self.control_panel = ControlPanel()
+            self.close()
+        elif account == "管理员" and password == "456":
+            self.control_panel = ControlPanel()
+            self.close()
+        else: pass
 
 
 class ControlPanel(QMainWindow):
@@ -26,6 +43,8 @@ class ControlPanel(QMainWindow):
         super().__init__()
         self.ui = Ui_ControlPanel()
         self.ui.setupUi(self)
+        # self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
         
         self.show()
 
