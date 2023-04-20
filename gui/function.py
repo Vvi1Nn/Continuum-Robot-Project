a7b1b6af2a8e2b1ba4dbce726399539ca88c7698
Vvi1Nn <2638569699@qq.com>
2023-04-20 23:30:49 +0800

    微调

--- gui/function.py
+++ gui/function.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import QThread, QMutex, QObject, pyqtSignal
 
 from ui_login import Ui_MainWindow as Ui_LoginPanel
 from ui_control import Ui_MainWindow as Ui_ControlPanel
@@ -43,6 +43,7 @@ class LoginPanel(QMainWindow):
             self.control_panel = ControlPanel(account)
         else: pass
 
+
 class ControlPanel(QMainWindow):
     def __init__(self, account) -> None:
         super().__init__()
@@ -51,8 +52,8 @@ class ControlPanel(QMainWindow):
         self.show()
         
         self.__is_admin = False if account == "测试人员" else True
-        self.__usbcan_0 = None
-        self.__usbcan_1 = None
+        self.__usbcan_0 = UsbCan(channel="0")
+        self.__usbcan_1 = UsbCan(channel="1")
 
         self.set_menu_jumping() # 设置菜单的按钮跳转
 
@@ -61,62 +62,35 @@ class ControlPanel(QMainWindow):
         self.set_usbcan_jumping() # 设置usbcan初始化部分的按钮跳转
         self.set_motor_jumping() # 设置motor初始化部分的按钮跳转
 
-
-    def set_menu_jumping(self) -> None:
-        self.ui.bt_init.clicked.connect(self.__jump_to_init_page)
-        self.ui.bt_control.clicked.connect(self.__jump_to_control_page)
-        self.ui.bt_log.clicked.connect(self.__jump_to_log_page)
-    def __jump_to_init_page(self) -> None:
-        self.ui.stackedWidget.setCurrentIndex(0)
-    def __jump_to_control_page(self) -> None:
-        self.ui.stackedWidget.setCurrentIndex(1)
-    def __jump_to_log_page(self) -> None:
-        self.ui.stackedWidget.setCurrentIndex(2)
-
-
-    def set_default_show(self) -> None:
-        self.__enable_menu(True, False, False) # menu 初始化激活 控制失效 日志失效
-        
-        self.__jump_to_init_page() # 显示初始化页面
-
-        self.__enable_open_device(True, "打开设备") # usbcan
-        self.__enable_channel0(False, "打开通道0", False, "重置通道0")
-        self.__enable_channel1(False, "打开通道1", False, "重置通道1")
-        self.__enable_close_device(False, "关闭设备")
-
-        self.__enable_default() # motor
-        self.__enable_set_param(False, "默认值1000", "默认值10000", "默认值100")
-        self.__enable_save_param(False)
-        self.__enable_init_motor(False, "生效")
-    def __enable_menu(self, init, control, log):
+    def enable_menu(self, init, control, log):
         self.ui.bt_init.setEnabled(init)
         self.ui.bt_control.setEnabled(control)
         self.ui.bt_log.setEnabled(log)
-    def __enable_open_device(self, flag, text):
+    def enable_open_device(self, flag, text):
         self.ui.bt_open.setEnabled(flag)
         self.ui.bt_open.setText(text)
-    def __enable_channel0(self, flag_open, text_open, flag_reset, text_reset):
+    def enable_channel0(self, flag_open, text_open, flag_reset, text_reset):
         self.ui.bx_rate0.setEnabled(flag_open)
         self.ui.bt_channel0.setEnabled(flag_open)
         self.ui.bt_channel0.setText(text_open)
         self.ui.bt_reset0.setEnabled(flag_reset)
         self.ui.bt_reset0.setText(text_reset)
-    def __enable_channel1(self, flag_open, text_open, flag_reset, text_reset):
+    def enable_channel1(self, flag_open, text_open, flag_reset, text_reset):
         self.ui.bx_rate1.setEnabled(flag_open)
         self.ui.bt_channel1.setEnabled(flag_open)
         self.ui.bt_channel1.setText(text_open)
         self.ui.bt_reset1.setEnabled(flag_reset)
         self.ui.bt_reset1.setText(text_reset)
-    def __enable_close_device(self, flag, text):
+    def enable_close_device(self, flag, text):
         self.ui.bt_close.setEnabled(flag)
         self.ui.bt_close.setText(text)
-    def __enable_default(self):
+    def enable_default(self):
         self.ui.r_pos.setChecked(True) # 选择位置模式
         self.ui.r_pos.setEnabled(False)
         self.ui.r_vel.setEnabled(False) # 禁用模式选择
         self.ui.tx_speed.setEnabled(False)
         self.ui.le_speed.setEnabled(False) # 禁用速度设置
-    def __enable_set_param(self, flag, text0, text1, text2):
+    def enable_set_param(self, flag, text0, text1, text2):
         self.ui.le_acc.clear()
         self.ui.tx_acc.setEnabled(flag)
         self.ui.le_acc.setEnabled(flag)
@@ -129,57 +103,82 @@ class ControlPanel(QMainWindow):
         self.ui.tx_vel.setEnabled(flag)
         self.ui.le_vel.setEnabled(flag)
         self.ui.le_vel.setPlaceholderText(text2)
-    def __enable_save_param(self, flag):
+    def enable_save_param(self, flag):
         self.ui.bt_save.setEnabled(flag)
-    def __enable_init_motor(self, flag, text):
+    def enable_init_motor(self, flag, text):
         self.ui.bt_launch.setEnabled(flag)
         self.ui.bt_launch.setText(text)
     
+    
+    def set_menu_jumping(self) -> None:
+        self.ui.bt_init.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
+        self.ui.bt_control.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
+        self.ui.bt_log.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
+
+    def set_default_show(self) -> None:
+        self.enable_menu(True, False, False) # menu 初始化激活 控制失效 日志失效
+        
+        self.ui.stackedWidget.setCurrentIndex(0)
+
+        self.enable_open_device(True, "打开设备") # usbcan
+        self.enable_channel0(False, "打开通道0", False, "重置通道0")
+        self.enable_channel1(False, "打开通道1", False, "重置通道1")
+        self.enable_close_device(False, "关闭设备")
+
+        self.enable_default() # motor
+        self.enable_set_param(False, "默认值1000", "默认值10000", "默认值100")
+        self.enable_save_param(False)
+        self.enable_init_motor(False, "生效")
 
     def set_usbcan_jumping(self) -> None:
-        self.ui.bt_open.clicked.connect(self.__interface_open_usbcan)
-        self.ui.bt_channel0.clicked.connect(self.__interface_start_channel_0)
-        self.ui.bt_channel1.clicked.connect(self.__interface_start_channel_1)
-        self.ui.bt_reset0.clicked.connect(self.__interface_reset_cannel_0)
-        self.ui.bt_reset1.clicked.connect(self.__interface_reset_cannel_1)
-        self.ui.bt_close.clicked.connect(self.__interface_close_usbcan)
-    def __interface_open_usbcan(self) -> None:
-        UsbCan.open_device()
-        if UsbCan.is_open:
-            self.__enable_open_device(False, "设备已打开")
-            self.__enable_close_device(True, "关闭设备")
-            self.__enable_channel0(True, "打开通道0", False, "重置通道0")
-            self.__enable_channel1(True, "打开通道1", False, "重置通道1")
-    def __interface_start_channel_0(self) -> None:
-        self.__usbcan_0 = UsbCan("0", self.ui.bx_rate0.currentText())
-        if self.__usbcan_0.is_init and self.__usbcan_0.is_start:
-            self.__enable_channel0(False, "通道0已打开", True, "重置通道0")
-            if self.__is_admin:
-                self.__enable_save_param(True) # 激活 保存
-                self.__enable_set_param(True, "默认值1000", "默认值10000", "默认值100") # 激活输入
-            else:
-                Motor.config(self.__usbcan_0) # 直接保存默认参数
-            self.__enable_init_motor(True, "生效") # 激活 生效
-    def __interface_start_channel_1(self) -> None:
-        self.__usbcan_1 = UsbCan("1", self.ui.bx_rate1.currentText())
-        if self.__usbcan_1.is_init and self.__usbcan_1.is_start:
-            self.__enable_channel1(False, "通道1已打开", True, "重置通道1")
-            pass # 其他的配置 后续补齐
-    def __interface_reset_cannel_0(self) -> None:
-        self.__usbcan_0.reset_can()
-    def __interface_reset_cannel_1(self) -> None:
-        self.__usbcan_1.reset_can()
-    def __interface_close_usbcan(self) -> None:
-        UsbCan.close_device()
-        if not UsbCan.is_open:
-            self.__enable_menu(True, False, False)
-            self.__enable_open_device(True, "打开设备")
-            self.__enable_channel0(False, "打开通道0", False, "重置通道0")
-            self.__enable_channel1(False, "打开通道1", False, "重置通道1")
-            self.__enable_close_device(False, "设备已关闭")
-            self.__enable_set_param(False, "默认值1000", "默认值10000", "默认值100")
-            self.__enable_save_param(False)
-            self.__enable_init_motor(False, "生效")
+        def open_usbcan():
+            UsbCan.open_device()
+            if UsbCan.is_open:
+                self.enable_open_device(False, "设备已打开")
+                self.enable_close_device(True, "关闭设备")
+                self.enable_channel0(True, "打开通道0", False, "重置通道0")
+                self.enable_channel1(True, "打开通道1", False, "重置通道1")
+        def start_channel_0():
+            self.__usbcan_0.timer = self.ui.bx_rate0.currentText()
+            self.__usbcan_0.init_can()
+            self.__usbcan_0.start_can()
+            if self.__usbcan_0.is_start:
+                self.enable_channel0(False, "通道0已打开", True, "重置通道0")
+                if self.__is_admin:
+                    self.enable_save_param(True) # 激活 保存
+                    self.enable_set_param(True, "默认值1000", "默认值10000", "默认值100") # 激活输入
+                else:
+                    Motor.config(self.__usbcan_0) # 直接保存默认参数
+                self.enable_init_motor(True, "生效") # 激活 生效
+        def start_channel_1():
+            self.__usbcan_1.timer = self.ui.bx_rate1.currentText()
+            self.__usbcan_1.init_can()
+            self.__usbcan_1.start_can()
+            if self.__usbcan_1.is_start:
+                self.enable_channel1(False, "通道1已打开", True, "重置通道1")
+                pass # 其他的配置 后续补齐
+        def reset_cannel_0():
+            self.__usbcan_0.reset_can()
+        def reset_cannel_1():
+            self.__usbcan_1.reset_can()
+        def close_usbcan():
+            UsbCan.close_device()
+            if not UsbCan.is_open:
+                self.enable_menu(True, False, False)
+                self.enable_open_device(True, "打开设备")
+                self.enable_channel0(False, "打开通道0", False, "重置通道0")
+                self.enable_channel1(False, "打开通道1", False, "重置通道1")
+                self.enable_close_device(False, "设备已关闭")
+                self.enable_set_param(False, "默认值1000", "默认值10000", "默认值100")
+                self.enable_save_param(False)
+                self.enable_init_motor(False, "生效")
+        self.ui.bt_open.clicked.connect(lambda: open_usbcan())
+        self.ui.bt_channel0.clicked.connect(lambda: start_channel_0())
+        self.ui.bt_channel1.clicked.connect(lambda: start_channel_1())
+        self.ui.bt_reset0.clicked.connect(lambda: reset_cannel_0())
+        self.ui.bt_reset1.clicked.connect(lambda: reset_cannel_1())
+        self.ui.bt_close.clicked.connect(lambda: close_usbcan())
+    
     def set_motor_jumping(self):
         self.ui.bt_save.clicked.connect(self.__interface_save_config)
         self.ui.bt_launch.clicked.connect(self.__interface_init_motor)
@@ -191,10 +190,10 @@ class ControlPanel(QMainWindow):
         vel = self.ui.le_vel.text()
         if vel == "": vel = "100"
         Motor.config(device=self.__usbcan_0, acceleration=int(acc), deceleration=int(dec), velocity=int(vel))
-        self.__enable_set_param(True, "当前值{}".format(Motor.acceleration), "当前值{}".format(Motor.deceleration), "当前值{}".format(Motor.velocity))
+        self.enable_set_param(True, "当前值{}".format(Motor.acceleration), "当前值{}".format(Motor.deceleration), "当前值{}".format(Motor.velocity))
     def __interface_init_motor(self):
-        self.__enable_set_param(False, "当前值{}".format(Motor.acceleration), "当前值{}".format(Motor.deceleration), "当前值{}".format(Motor.velocity))
-        self.__enable_save_param(False)
+        self.enable_set_param(False, "当前值{}".format(Motor.acceleration), "当前值{}".format(Motor.deceleration), "当前值{}".format(Motor.velocity))
+        self.enable_save_param(False)
         self.__motor_1 = Motor(1)
         self.__motor_2 = Motor(2)
         # self.__motor_3 = Motor(3)
@@ -205,5 +204,5 @@ class ControlPanel(QMainWindow):
         # self.__motor_8 = Motor(8)
         # self.__motor_9 = Motor(9)
         # self.__motor_10 = Motor(10)
-        self.__enable_init_motor(False, "成功")
-        self.__enable_menu(True, True, False)
+        self.enable_init_motor(False, "成功")
+        self.enable_menu(True, True, False)
