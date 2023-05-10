c7e1cecb2ef07a18a5b3cd56e13eeddfdcfbf0c6
Vvi1Nn <2638569699@qq.com>
2023-05-10 23:23:30 +0800

    新

---
+++ gui/start_new.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from function_new import LoginPanel
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = LoginPanel()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
