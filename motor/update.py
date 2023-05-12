a4102e27909503db92c8120d2160e1896698e738
Vvi1Nn <2638569699@qq.com>
2023-05-12 19:08:02 +0800

    状态更新线程

--- motor/update.py
+++ motor/update.py
@@ -1,26 +1,24 @@
 # -*- coding:utf-8 -*-
 
 
-''' update.py 更新电机状态 v1.0 '''
+''' update.py 更新电机状态 v1.1 '''
 
 
-from PyQt5.QtCore import QThread, pyqtSignal, QMutex
+from PyQt5.QtCore import QThread, pyqtSignal
 from motor.function import Motor
 
 
 class MotorUpdateThread(QThread):
-    button_signal_0 = pyqtSignal(int)
+    update_signal = pyqtSignal(int)
     
-    def __init__(self, mutex: QMutex) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.__is_stop = False
-        self.__lock = mutex
     
     def run(self):
         while not self.__is_stop:
-            self.__lock.lock()
-            Motor.resolve_tpdo_msg()
-            self.__lock.unlock()
+            node_id = Motor.update_motor_status()
+            if node_id != 0: self.update_signal.emit(node_id)
     
     def stop(self):
         self.__is_stop = True
