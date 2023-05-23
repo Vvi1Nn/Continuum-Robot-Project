b6bfd92d2ed6dc4fd859d45bb21c9bafa0d9b46e
Vvi1Nn <2638569699@qq.com>
2023-05-23 22:04:42 +0800

    力传感器数据读取 v1.0

--- sensor/function.py
+++ sensor/function.py
@@ -18,6 +18,7 @@ class SensorUpdateThread(QThread):
         self.update_signal.connect(slot_function)
     
     def run(self):
+        Sensor.update_init()
         while not self.__is_stop:
             self.__mutex.lock()
             Sensor.update_force()
@@ -33,9 +34,10 @@ class Sensor():
     __sensor_dict = {}
     __msg = [0x49, 0xAA, 0x0D, 0x0A]
     
-    def __init__(self, node_id):
+    def __init__(self, node_id) -> None:
         self.__node_id = node_id
         self.__sensor_dict[node_id] = self
+        self.__init = 0
         self.force = 0
 
     @classmethod
@@ -43,29 +45,55 @@ class Sensor():
         cls.__device = device
         return cls
     
-    def get_force(self):
+    def get_init(self, count=50) -> None:
+        value = 0
+        times = count
+        while times != 0:
+            if self.__device.send(self.__node_id, [self.__msg], data_len="sensor"):
+                time.sleep(0.01)
+                ret = self.__device.read_buffer(1)
+                if ret != None:
+                    [num, msg] = ret
+                    if num!= 0:
+                        value += self.__hex_list_to_float([msg[0].Data[2], msg[0].Data[3], msg[0].Data[4], msg[0].Data[5]])
+                        times -= 1
+        self.__init = value / count
+
+    @staticmethod
+    def update_init() -> None:
+        for sensor in Sensor.__sensor_dict.values():
+            sensor.get_init()
+
+    def get_force(self) -> None:
         if self.__device.send(self.__node_id, [self.__msg], data_len="sensor"):
             time.sleep(0.01)
             ret = self.__device.read_buffer(1)
             if ret != None:
                 [num, msg] = ret
                 if num!= 0:
-                    self.force = self.__hex_list_to_int([msg[0].Data[2], msg[0].Data[3], msg[0].Data[4], msg[0].Data[5]])
+                    self.force = round(self.__hex_list_to_float([msg[0].Data[2], msg[0].Data[3], msg[0].Data[4], msg[0].Data[5]]) - self.__init, 2)
+                    print(self.force)
     
     @staticmethod
-    def update_force():
+    def update_force() -> None:
         for sensor in Sensor.__sensor_dict.values():
             sensor.get_force()
 
     @staticmethod
-    def __hex_list_to_int(data_list) -> int:
+    def __hex_list_to_float(data_list) -> float:
         data_str = ""
         for i in range(len(data_list)):
             data_bin = bin(data_list[i])[2:] # 首先转换为bin 去除0b
             data_bin = '0' * (8 - len(data_bin)) + data_bin # 头部补齐
             data_str = data_bin + data_str # 拼接
-        # 首位是0 正数
-        if int(data_str[0]) == 0: return int(data_str, 2)
-        # 首位是1 负数
-        else: return - ((int(data_str, 2) ^ 0xFFFFFFFF) + 1)
-    
+        # 确定符号
+        if data_str[0] == "0": sign = 1
+        else: sign = -1
+        # 确定整数
+        power = 2 ** (int(data_str[1:9], 2) - 127)
+        # 确定小数
+        decimal = 1
+        for i, num in enumerate(data_str[9:]):
+            if num == "1": decimal = 2 ** (- (i + 1)) + decimal
+        # 结果
+        return sign * power * decimal
