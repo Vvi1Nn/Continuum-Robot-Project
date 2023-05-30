1da3f41ea66f93d45444525afba7325de3723346
Vvi1Nn <2638569699@qq.com>
2023-05-30 22:52:19 +0800

    移除更新函数 给状态控制函数添加延时 保证数据发送准确

--- motor/function.py
+++ motor/function.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 
-''' function.py 步进电机功能函数 v2.8 '''
+''' function.py 步进电机功能函数 v2.8 移除更新函数 给状态控制函数添加延时 保证数据发送准确'''
 
 
 # 添加模块路径
@@ -206,6 +206,7 @@ class Motor(CanOpenBusProcessor):
         for motor in cls.motor_dict.values():
             if motor.set_servo_status("servo_close"):
                 print("\033[0;32m[Motor {}] release brake\033[0m".format(motor.node_id))
+                time.sleep(0.001)
                 continue
             print("\033[0;31m[Motor {}] release brake failed\033[0m".format(motor.node_id))
 
@@ -216,6 +217,7 @@ class Motor(CanOpenBusProcessor):
         for motor in cls.motor_dict.values():
             if motor.set_servo_status("servo_ready/stop"):
                 print("\033[0;32m[Motor {}] lock brake\033[0m".format(motor.node_id))
+                time.sleep(0.001)
                 continue
             print("\033[0;31m[Motor {}] lock brake failed\033[0m".format(motor.node_id))
 
@@ -226,6 +228,7 @@ class Motor(CanOpenBusProcessor):
         for motor in cls.motor_dict.values():
             if motor.set_servo_status("quick_stop"):
                 print("\033[0;32m[Motor {}] quick stop\033[0m".format(motor.node_id))
+                time.sleep(0.001)
                 continue
             print("\033[0;31m[Motor {}] quick stop failed\033[0m".format(motor.node_id))
 
@@ -239,43 +242,43 @@ class Motor(CanOpenBusProcessor):
                 continue
             print("\033[0;31m[Motor {}] reset failed\033[0m".format(motor.node_id))
 
-    ''' hex列表转换为int '''
-    @staticmethod
-    def __hex_list_to_int(data_list) -> int:
-        data_str = ""
-        for i in range(len(data_list)):
-            data_bin = bin(data_list[i])[2:] # 首先转换为bin 去除0b
-            data_bin = '0' * (8 - len(data_bin)) + data_bin # 头部补齐
-            data_str = data_bin + data_str # 拼接
-        # 首位是0 正数
-        if int(data_str[0]) == 0: return int(data_str, 2)
-        # 首位是1 负数
-        else: return - ((int(data_str, 2) ^ 0xFFFFFFFF) + 1)
+    # ''' hex列表转换为int '''
+    # @staticmethod
+    # def __hex_list_to_int(data_list) -> int:
+    #     data_str = ""
+    #     for i in range(len(data_list)):
+    #         data_bin = bin(data_list[i])[2:] # 首先转换为bin 去除0b
+    #         data_bin = '0' * (8 - len(data_bin)) + data_bin # 头部补齐
+    #         data_str = data_bin + data_str # 拼接
+    #     # 首位是0 正数
+    #     if int(data_str[0]) == 0: return int(data_str, 2)
+    #     # 首位是1 负数
+    #     else: return - ((int(data_str, 2) ^ 0xFFFFFFFF) + 1)
     
-    ''' 更新状态 '''
-    @classmethod
-    def update_motor_status(cls) -> int:
-        ret = cls.device.read_buffer(1, wait_time=0)
-        if ret != None:
-            [num, msg] = ret
-            for i in range(num):
-                if msg[i].ID > 0x180 and msg[i].ID < 0x200:
-                    node_id = msg[i].ID-0x180
-                    status = cls.__hex_list_to_int([msg[i].Data[0]]) # 状态字
-                    for key in protocol.STATUS_WORD: # 遍历字典关键字
-                        for r in protocol.STATUS_WORD[key]: # 在每一个关键字对应的列表中 核对数值
-                            if status == r:
-                                cls.motor_dict[node_id].motor_status = key # 更新电机的伺服状态
-                                return node_id
-                elif msg[i].ID > 0x280 and msg[i].ID < 0x300:
-                    node_id = msg[i].ID-0x280
-                    position = cls.__hex_list_to_int([msg[i].Data[j] for j in range(0,4)]) # 当前位置
-                    speed = cls.__hex_list_to_int([msg[i].Data[j] for j in range(4,8)]) # 当前速度
-                    cls.motor_dict[node_id].current_position = position
-                    cls.motor_dict[node_id].current_speed = speed
-                    return node_id
-                else: pass
-        return 0
+    # ''' 更新状态 '''
+    # @classmethod
+    # def update_motor_status(cls) -> int:
+    #     ret = cls.device.read_buffer(1, wait_time=0)
+    #     if ret != None:
+    #         [num, msg] = ret
+    #         for i in range(num):
+    #             if msg[i].ID > 0x180 and msg[i].ID < 0x200:
+    #                 node_id = msg[i].ID-0x180
+    #                 status = cls.__hex_list_to_int([msg[i].Data[0]]) # 状态字
+    #                 for key in protocol.STATUS_WORD: # 遍历字典关键字
+    #                     for r in protocol.STATUS_WORD[key]: # 在每一个关键字对应的列表中 核对数值
+    #                         if status == r:
+    #                             cls.motor_dict[node_id].motor_status = key # 更新电机的伺服状态
+    #                             return node_id
+    #             elif msg[i].ID > 0x280 and msg[i].ID < 0x300:
+    #                 node_id = msg[i].ID-0x280
+    #                 position = cls.__hex_list_to_int([msg[i].Data[j] for j in range(0,4)]) # 当前位置
+    #                 speed = cls.__hex_list_to_int([msg[i].Data[j] for j in range(4,8)]) # 当前速度
+    #                 cls.motor_dict[node_id].current_position = position
+    #                 cls.motor_dict[node_id].current_speed = speed
+    #                 return node_id
+    #             else: pass
+    #     return 0
     
     # ''' 单电机运动测试 位置控制模式 相对运行 立即模式 可实现点击按钮动作 '''
     # def start_joint_forward(self):
