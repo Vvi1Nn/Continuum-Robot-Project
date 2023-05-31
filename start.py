8bc8a5aa3c74b136d23c859b9d45dc9843ef9732
Vvi1Nn <2638569699@qq.com>
2023-05-31 22:13:47 +0800

    增加程序结束后的操作

--- start.py
+++ start.py
@@ -1,17 +1,24 @@
 # -*- coding:utf-8 -*-
 
 
-import sys
+import sys, time
 
 from PyQt5.QtWidgets import QApplication
 
 from gui.function_v3 import LoginPanel
+from motor.function import Motor
 
 
 def main():
     app = QApplication(sys.argv)
     win = LoginPanel()
-    sys.exit(app.exec_())
+    if not app.exec_():
+        print("=============================================================")
+        print("\033[0;33mShuting down, waiting for homing ...\033[0m")
+        if Motor.homing(): print("\033[0;32mDone!\033[0m")
+        else: print("\033[0;31mFailed...\033[0m")
+        print("=============================================================")
+        sys.exit()
 
 if __name__ == "__main__":
     main()
