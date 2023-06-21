97b6e2059f4efca4e2174be84d25fb91f2c6c7da
Vvi1Nn <2638569699@qq.com>
2023-06-21 20:36:15 +0800

    优化程序终止后的操作

--- start.py
+++ start.py
@@ -14,10 +14,17 @@ def main():
     win = LoginPanel()
     if not app.exec_():
         print("=============================================================")
-        print("\033[0;33mShuting down, waiting for homing ...\033[0m")
-        if Motor.homing(): print("\033[0;32mDone!\033[0m")
-        else: print("\033[0;31mFailed...\033[0m")
-        print("=============================================================")
+        print("\033[0;33mShuting down, waiting for processing ...\033[0m")
+        if win.control_panel.motor_is_running:
+            win.control_panel.quick_stop()
+            time.sleep(1)
+            print("\033[0;32mquick stop!\033[0m")
+            win.control_panel.stop_pdo()
+            time.sleep(1)
+            print("\033[0;32mstop pdo!\033[0m")
+        else:
+            if Motor.homing(): print("\033[0;32mDone!\033[0m")
+            else: print("\033[0;31mFailed...\033[0m")
         sys.exit()
 
 if __name__ == "__main__":
