bbf93e9b6db82f2575dfe176f8b4a2a9594dd12d
Vvi1Nn <2638569699@qq.com>
2023-06-22 23:13:02 +0800

    需要修改PDO没有开启的时候无法homing的bug

--- start.py
+++ start.py
@@ -15,16 +15,35 @@ def main():
     if not app.exec_():
         print("=============================================================")
         print("\033[0;33mShuting down, waiting for processing ...\033[0m")
-        if win.control_panel.motor_is_running:
+        
+        if win.control_panel.usbcan_is_running:
+            if not win.control_panel.motor_is_running:
+                win.control_panel.start_pdo()
+                time.sleep(2)
+            else: pass
+
             win.control_panel.quick_stop()
-            time.sleep(1)
-            print("\033[0;32mquick stop!\033[0m")
+            time.sleep(0.1)
+
+            win.control_panel.homing_pdo()
+            print("\033[0;32mMOTOR 10 HOMED\033[0m")
+
             win.control_panel.stop_pdo()
             time.sleep(1)
-            print("\033[0;32mstop pdo!\033[0m")
-        else:
-            if Motor.homing(): print("\033[0;32mDone!\033[0m")
-            else: print("\033[0;31mFailed...\033[0m")
+                
+            
+            if win.control_panel.io_is_running:
+                win.control_panel.close_module()
+                print("\033[0;32mIO STOP\033[0m")
+            else: pass
+
+            if win.control_panel.sensor_is_running:
+                win.control_panel.stop_force()
+                print("\033[0;32mSENSOR STOP\033[0m")
+            else: pass
+        else: pass
+        
+        print("=============================================================")
         sys.exit()
 
 if __name__ == "__main__":
