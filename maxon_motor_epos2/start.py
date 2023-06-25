55dc245952bb12d77c92350ea036525b6591724f
Vvi1Nn <2638569699@qq.com>
2023-06-25 21:39:26 +0800

    入口

---
+++ maxon_motor_epos2/start.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+
+import sys, time
+
+from PyQt5.QtWidgets import QApplication
+
+# 添加模块路径
+import sys, os, time
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from maxon_motor_epos2.gui import ControlPanel
+from maxon_motor_epos2.motor import Motor
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = ControlPanel()
+    if not app.exec_():
+        print("=============================================================")
+        print("\033[0;33mShuting down, waiting for processing ...\033[0m")
+        
+        
+        
+        print("=============================================================")
+        sys.exit()
+
+if __name__ == "__main__":
+    main()
+    
