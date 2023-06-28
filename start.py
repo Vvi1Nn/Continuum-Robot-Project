dbe679e7fde1f10984c30a634d704cdc6904fa23
Vvi1Nn <2638569699@qq.com>
2023-06-28 20:44:54 +0800

    取消退出操作

--- start.py
+++ start.py
@@ -16,32 +16,32 @@ def main():
         print("=============================================================")
         print("\033[0;33mShuting down, waiting for processing ...\033[0m")
         
-        if win.control_panel.usbcan_is_running:
-            if not win.control_panel.motor_is_running:
-                win.control_panel.start_pdo()
-                time.sleep(2)
-            else: pass
+        # if win.control_panel.usbcan_is_running:
+        #     if not win.control_panel.motor_is_running:
+        #         win.control_panel.start_pdo()
+        #         time.sleep(2)
+        #     else: pass
 
-            win.control_panel.quick_stop()
-            time.sleep(0.1)
+        #     win.control_panel.quick_stop()
+        #     time.sleep(0.1)
 
-            win.control_panel.homing_pdo()
-            print("\033[0;32mMOTOR 10 HOMED\033[0m")
+        #     win.control_panel.homing_pdo()
+        #     print("\033[0;32mMOTOR 10 HOMED\033[0m")
 
-            win.control_panel.stop_pdo()
-            time.sleep(1)
+        #     win.control_panel.stop_pdo()
+        #     time.sleep(1)
                 
             
-            if win.control_panel.io_is_running:
-                win.control_panel.close_module()
-                print("\033[0;32mIO STOP\033[0m")
-            else: pass
+        #     if win.control_panel.io_is_running:
+        #         win.control_panel.close_module()
+        #         print("\033[0;32mIO STOP\033[0m")
+        #     else: pass
 
-            if win.control_panel.sensor_is_running:
-                win.control_panel.stop_force()
-                print("\033[0;32mSENSOR STOP\033[0m")
-            else: pass
-        else: pass
+        #     if win.control_panel.sensor_is_running:
+        #         win.control_panel.stop_force()
+        #         print("\033[0;32mSENSOR STOP\033[0m")
+        #     else: pass
+        # else: pass
         
         print("=============================================================")
         sys.exit()
