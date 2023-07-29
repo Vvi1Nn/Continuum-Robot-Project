7e3d6d81394bb841906cbb2bc0dc47fe8ab2e082
Vvi1Nn <2638569699@qq.com>
2023-07-29 17:29:59 +0800

    joystick

--- continuum_robot/joystick.py
+++ continuum_robot/joystick.py
@@ -4,12 +4,12 @@
 ''' joystick.py '''
 
 
-import pygame
-from PyQt5.QtCore import QThread, pyqtSignal
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from pygame.locals import *
-from OpenGL.GL import *
-from OpenGL.GLU import *
+import pygame
+
+from PyQt5.QtCore import QThread, pyqtSignal
 
 
 class Joystick(QThread):
@@ -31,8 +31,11 @@ class Joystick(QThread):
     axis_signal_3 = pyqtSignal(float)
     hat_signal_0 = pyqtSignal(tuple)
     
-    def __init__(self) -> None:
+    def __init__(self, robot) -> None:
         super().__init__()
+        
+        self.robot = robot
+
         self.__is_joystick = False
         self.__is_stop = False
         
@@ -42,20 +45,14 @@ class Joystick(QThread):
         pygame.init() # 初始化模块
 
         # 界面
-        # self.screen = pygame.display.set_mode([800, 500])
-        self.screen = pygame.display.set_mode((240, 180), DOUBLEBUF | OPENGL)
+        self.screen = pygame.display.set_mode([800, 500])
+        # self.screen = pygame.display.set_mode((240, 180))
         pygame.display.set_caption("遥操作界面")
         self.screen.fill((255, 255, 255))
 
         self.font = pygame.font.SysFont(None, 40) # 字体
 
         self.clock = pygame.time.Clock() # 时钟
-
-        glMatrixMode(GL_PROJECTION)
-        gluPerspective(45, 240 / 180, 0.1, 50.0)
-        glMatrixMode(GL_MODELVIEW)
-        glLoadIdentity()
-        glTranslatef(0, 0, -5.0)
 
         # 等待joystick插入
         while not self.__is_joystick:
@@ -82,53 +79,40 @@ class Joystick(QThread):
             # 检测
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: self.__is_stop = True
-                # elif event.type == pygame.JOYBUTTONDOWN: print("Button pressed")
-                # elif event.type == pygame.JOYBUTTONUP: print("Button released")
-                # elif event.type == pygame.JOYHATMOTION: print("Joystick hat")
-                # elif event.type == pygame.JOYAXISMOTION: print("Joystick axis")
-                else: pass
             
             # 显示
             self.screen.fill((255,255,255))
             x_current, y_current = 10, 10
             self.__print("Joystick name: {}".format(self.joystick.get_name()), x=x_current, y=y_current)
-            for i in range(self.joystick.get_numbuttons()):
+            
+            button_num = self.joystick.get_numbuttons()
+            axis_num = self.joystick.get_numaxes()
+            hat_num = self.joystick.get_numhats()
+            
+            for i in range(button_num):
                 getattr(self, f"button_signal_{i}").emit(self.joystick.get_button(i))
                 y_current += 40
                 self.__print("Button {}:  {}".format(i, True if self.joystick.get_button(i) else False), x=x_current, y=y_current)
             x_current, y_current = 310, 10
-            for i in range(self.joystick.get_numaxes()):
+            
+            for i in range(axis_num):
                 getattr(self, f"axis_signal_{i}").emit(self.joystick.get_axis(i))
                 y_current += 40
                 self.__print("Axis {}:  {:.6f}".format(i, self.joystick.get_axis(i)), x=x_current, y=y_current)
             x_current, y_current = 610, 10
-            for i in range(self.joystick.get_numhats()):
+            
+            for i in range(hat_num):
                 y_current += 40
                 self.__print("Hat {}:  {}".format(i, str(self.joystick.get_hat(i))), x=x_current, y=y_current)
             
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            glBegin(GL_LINES)
-            # X axis
-            glColor3f(1, 0, 0)
-            glVertex3f(0, 0, 0)
-            glVertex3f(1, 0, 0)
-            # Y axis
-            glColor3f(0, 1, 0)
-            glVertex3f(0, 0, 0)
-            glVertex3f(0, 1, 0)
-            # Z axis
-            glColor3f(0, 0, 1)
-            glVertex3f(0, 0, 0)
-            glVertex3f(0, 0, 1)
-            glEnd()
-            if self.joystick.get_button(0) == 1:
-                glRotatef(0 if abs(self.joystick.get_axis(1))<0.01 else self.joystick.get_axis(1), 1, 0, 0)
-                glRotatef(0 if abs(self.joystick.get_axis(0))<0.01 else self.joystick.get_axis(0), 0, 1, 0)
-                glTranslatef(0.01*self.joystick.get_hat(0)[0], 0                               , 0)
-                glTranslatef(0                               , 0.01*self.joystick.get_hat(0)[1], 0)
-                glTranslatef(0                               , 0                               , 0.01*self.joystick.get_button(2))
-                glTranslatef(0                               , 0                               , -0.01*self.joystick.get_button(3))
-
+            # 操作
+            if self.joystick.get_button(0):
+                if self.joystick.get_button(2) and not self.joystick.get_button(3): # out
+                    kappa_d = self.joystick.get_axis(1) * 0.002
+                    phi_d = self.joystick.get_axis(2) * 0.5
+                elif self.joystick.get_button(3) and not self.joystick.get_button(2): # mid
+                    ...
+            
             # 刷新
             pygame.display.flip()
             self.clock.tick(20)
