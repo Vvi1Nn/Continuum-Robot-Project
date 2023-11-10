176a957625f6c601c7614cf355c0b82843411d48
Vvi1Nn <2638569699@qq.com>
2023-11-10 21:53:57 +0800

    Joystick

--- continuum_robot/joystick.py
+++ continuum_robot/joystick.py
@@ -4,9 +4,6 @@
 ''' joystick.py '''
 
 
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import pygame
 
 from PyQt5.QtCore import QThread, pyqtSignal
