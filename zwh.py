efe57d8c8488cd40d71402c365c6c01a496ff215
Vvi1Nn <2638569699@qq.com>
2023-11-16 23:33:59 +0800

    测试

--- zwh.py
+++ zwh.py
@@ -18,11 +18,42 @@ print("\033[7;35m紫红+反白\033[0m")
 print("\033[0;36m青蓝\033[0m")
 print("\033[8;37m白+不可见\033[0m")
 
-class A():
-    def __init__(self, name) -> None:
-        self.name = name
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
 
-a = {}
-for i in range(5):
-    a[i] = A(i)
-    print(a[i].name)
+# fig, ax = plt.subplots()
+# x=np.arange(1,10,0.1)
+# y=np.sin(x)
+# line, = ax.plot(x,y)
+
+# def update(frame):
+#     new_data = np.sin(x+frame)
+#     line.set_ydata(new_data)
+#     return line,
+# ani = FuncAnimation(fig, update, frames=np.linspace(0, 2*np.pi, 128), interval=50)
+# plt.show()
+# print("111")
+
+# from itertools import count
+# import random
+# x_vals = []
+# y_vals = []
+# index = count()
+# def animate(i):
+#     x = next(index)
+#     y = random.randint(0,10)
+#     x_vals.append(x)
+#     y_vals.append(y)
+#     if len(x_vals)>50:
+#         x_vals.pop(0)
+#         y_vals.pop(0)
+#     plt.cla()
+#     plt.plot(x_vals, y_vals)
+#     plt.tight_layout()
+# ani = FuncAnimation(plt.gcf(), animate, interval=50)
+# plt.tight_layout()
+# plt.show()
+
+section = "a"
+print(section == "a" or section == "b")
